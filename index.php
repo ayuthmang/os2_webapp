b234eecc4cefc2788fea7a6aa341b58a27467285
Ayuth Mangmesap <blackSource@windowslive.com>
2017-08-28 01:58:14 +0700

    init source

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <header>
+        <title>Simple PHP app</title>
+    </header>
+    <body>
+        <h1>Member</h1>
+        <?php
+            include 'query.php';
+            readMember();
+        ?>
+    </body>
+</html>
